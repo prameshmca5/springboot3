207ea49749fc7e13e9f733ed3f5a8b2570b65cea
prameshmca5 <rameshkp2@gmail.com>
2023-06-19 22:50:37 +0800

    init springboot

---
+++ src/main/java/com/ramesh/threespringboot/SpbootthreeApplication.java
@@ -0,0 +1,13 @@
+package com.ramesh.threespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootthreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbootthreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramesh/threespringboot/SpbootthreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramesh.threespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbootthreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
