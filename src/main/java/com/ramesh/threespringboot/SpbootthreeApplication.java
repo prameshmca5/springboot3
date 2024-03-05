1b0bf3f57f1d05e3daf8e8f8d6557160e3e5ae90
prameshmca5 <rameshkp2@gmail.com>
2024-03-05 16:06:03 +0800

    Update SpbootthreeApplication.java

--- src/main/java/com/ramesh/threespringboot/SpbootthreeApplication.java
+++ src/main/java/com/ramesh/threespringboot/SpbootthreeApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class SpbootthreeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpbootthreeApplication.class, args);
 	}
 
+ @GetMapping("/hello")
+  public String hello(){
+    return "Hello";
+  }	
 }
